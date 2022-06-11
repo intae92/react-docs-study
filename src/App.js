851a8d44f210a24b5565a39e8cfd0f584a7b921d
intae92 <kangit2013@gmail.com>
2022-06-11 16:08:44 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState, useRef } from "react";
 
 function App() {
+  const [isShow, setIsShow] = useState(false);
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsShow(true);
+    }, 1000);
+  }, []);
+  // console.log("count", count);
+
+  useEffect(() => {
+    setInterval(() => {
+      // setCount(count + 1);
+      // console.log("setInterval start");
+      // setCount((prev) => {
+      //   console.log("setCount", prev);
+      //   return prev + 1;
+      // });
+      setCount((prev) => prev + 1);
+    }, 1000);
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>부모{count}</div>
+      <div>{isShow && <Coke count={count} />}</div>
     </div>
   );
+}
+
+function Coke({ count }) {
+  console.log("coke", count);
+  const [text, setText] = useState("coke");
+
+  const handleClick = () => {
+    console.log("click count", count);
+    setText((prev) => prev + count);
+  };
+  return (
+    <div>
+      <div>자식{count}</div>
+      <div>text{text}</div>
+      <div>
+        <button onClick={handleClick}>자식 클릭</button>
+      </div>
+    </div>
+  );
+}
+
+function useInterval(callback, delay) {
+  const savedCallback = useRef();
+
+  // Remember the latest callback
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  useEffect(() => {
+    function tick() {}
+  }, [delay]);
 }
 
 export default App;
