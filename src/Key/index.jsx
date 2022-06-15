1be630a7d4f2d5f9e6802e8df82744202a3864fe
intae92 <kangit2013@gmail.com>
2022-06-15 16:28:20 +0900

    key 학습

--- src/Key/index.jsx
+++ src/Key/index.jsx
@@ -1,7 +1,43 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 
 const Key = () => {
-  return <div>Key</div>;
+  // const [numbers, setNumbers] = useState([0, 1, 2, 3, 4]);
+  const [numbers, setNumbers] = useState([
+    { id: "0", num: 0 },
+    { id: "1", num: 0 },
+    { id: "2", num: 0 },
+  ]);
+
+  // const handleAddFirst = () => {
+  //   setNumbers((x) => [x.length, ...x]);
+  // };
+
+  // const handleAddLast = () => {
+  //   setNumbers((x) => [...x, x.length]);
+  // };
+
+  // const handleRemove = (index) => {
+  //   setNumbers((x) => [...x.slice(0, index), ...x.slice(index + 1)]);
+  // };
+  // console.log(React.createElement("div"));
+
+  // useEffect(() => {
+  //   console.log("numbers", numbers);
+  // }, [numbers]);
+
+  return (
+    <div>
+      {numbers.map(({ id, num }) => (
+        <div key={id}>{num}</div>
+      ))}
+      <hr />
+      {numbers.map(({ id, num }) => (
+        <div key={id}>{num}</div>
+      ))}
+    </div>
+  );
 };
 
 export default Key;
